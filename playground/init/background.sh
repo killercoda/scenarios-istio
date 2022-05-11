19c8a49730a99137d41bfe762ea748cf632bf24c
Kim <kim@wuestkamp.com>
2022-05-11 18:09:47 +0200

    allow pods to run on controlplane

--- playground/init/background.sh
+++ playground/init/background.sh
@@ -9,6 +9,7 @@ touch /ks/.k8sfinished
 
 # allow pods to run on controlplane
 kubectl taint nodes controlplane node-role.kubernetes.io/master:NoSchedule-
+kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 
 # mark init finished
 touch /ks/.initfinished
